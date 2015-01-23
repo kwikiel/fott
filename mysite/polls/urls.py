48a4571b9bcbf17f36e3966e882df16d3f7d7972
root <random8902@gmail.com>
2015-01-23 12:48:23 -0500

    This is working version

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,12 @@ from polls import views
 
 
 urlpatterns = patterns ('',
+        #pools/5
         url(r'^$', views.index, name='index'),
+        url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+        #example thing that matches regex
+        url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+        #example /polls/5/vote/
+        url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+        
 )
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
