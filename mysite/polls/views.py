eba2dbb5dea7033516c135ebe550cb5073662d95
root <random8902@gmail.com>
2015-01-24 17:54:55 -0500

    Milestone 1.0 working app without tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,12 +14,12 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+#def detail(request, question_id):
+#    return HttpResponse("Pope  looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
